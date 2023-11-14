c4d4eafde33f03529553e0591c4c72a29cdfec8f
YoelTavger <yoel.tavger@gmail.com>
2023-11-14 13:39:31 +0200

    I finished logging in with Token and auth context

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface AuthContextProps {
+  isAuthenticated: User | null;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+interface User {
+  email: string;
+  token: string;
+}
+
+export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+const userString = localStorage.getItem("user");
+const user: User | null = userString ? JSON.parse(userString) : null;
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<User | null>(user);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
