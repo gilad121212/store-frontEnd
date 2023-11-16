0289df0e88d858a4315c06639e5341fd04e6d6b1
gilad sinai <gilad1586@gmail.com>
2023-11-16 11:16:26 +0200

    trghrth

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,34 +4,100 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import TypeProductCard from "../types/TypeProductCard";
+import { ShopingCartContext } from "../Context/ShopingCartContext";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CartItem } from "./shoppingCart";
 
-export default function ImgMediaCard(prop: TypeProductCard) {
+type Product = CartItem;
+
+
+
+export default function ImgMediaCard() {
+  const authContext = useContext(ShopingCartContext);
+  const cartItems = authContext?.shopingCart;
+  const setCartItems = authContext?.setShopingCart;
+
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+
+  const handleAddToCart = () => {
+    if (setCartItems && product) {
+      const existingProduct = cartItems?.find(
+        (item) => item.id === product.id
+      );
+      if (existingProduct) {
+        setCartItems((prevCartItems) =>
+        prevCartItems?.map((item) =>
+          item.id === existingProduct.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        ) || []
+      );
+      } else {
+        setCartItems((prevCartItems) => [
+          ...(prevCartItems || []),
+          product,
+        ]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(`http://127.0.0.1:3009/products/product/${id}`, requestOptions)
+      .then(async (res) => {
+        if (!res.ok) {
+          const errorText = await res.text();
+          throw new Error(
+            `HTTP error! Status: ${res.status}, Error: ${errorText}`
+          );
+        }
+        return res.json();
+      })
+      .then((result) => {
+        result.data.quantity = 1
+        setProduct(result.data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
     <div>
       <Card sx={{ maxWidth: 900, marginTop: "50px", marginLeft: "200px" }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="400"
-        image={prop.img}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h4" component="div">
-        {prop.title}
-        </Typography>
-        <Typography variant="h5" color="text.secondary" component="div">
-          praice: {prop.price}
-        </Typography>
-        <Typography variant="h6" color="text.secondary">
-          {prop.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">add to shopping cart</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="400"
+          image={product?.images[0]}
+        />
+        {product && (
+          <CardContent>
+          <Typography gutterBottom variant="h4" component="div">
+            {product.title}
+          </Typography>
+          <Typography variant="h5" color="text.secondary" component="div">
+            praice: {product.price}
+          </Typography>
+          <Typography variant="h6" color="text.secondary">
+            {product.description}
+          </Typography>
+        </CardContent>
+        )}
+        <CardActions>
+          <Button onClick={handleAddToCart} size="small">
+            add to shopping cart
+          </Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </div>
-    );
+  );
 }
